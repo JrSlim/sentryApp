8470a00e8894d22eefb4ce47bd66c6e36f54c6a6
slim <slimjerbii@gmail.com>
2020-03-23 00:45:12 +0100

    second comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import * as Sentry from "@sentry/browser";
 
-@NgModule({ 
+Sentry.init({
+  dsn: "http://761991c9023646e48685fdf320122979@localhost:9000/2"
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
+
+@NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
